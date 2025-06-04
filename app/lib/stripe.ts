4cc9dd9d544eb9d5517da26aeb9231826b21c115
FLCoolJ <jiirockjackson@yahoo.com>
2025-06-04 11:45:56 -0700

    Create stripe.ts

---
+++ app/lib/stripe.ts
@@ -0,0 +1,15 @@
+import Stripe from "stripe"
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-06-20",
+})
+
+export const getStripe = () => {
+  if (typeof window !== "undefined") {
+    const stripePromise = import("@stripe/stripe-js").then(({ loadStripe }) =>
+      loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!),
+    )
+    return stripePromise
+  }
+  return null
+}
